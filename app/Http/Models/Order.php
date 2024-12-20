51ba18a4967840249b79a9456a9d73e2ff3e38b0
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2024-12-20 13:40:14 -0300

    feat: adicionado models e migrations para pagamentos, pedidos, itens de pedidos, categorias, endereços, imagens de produtos e avaliações

---
+++ app/Http/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id');
+    }
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class, 'payment_id');
+    }
+}
