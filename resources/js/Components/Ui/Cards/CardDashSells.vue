cd73792fb1d6ba9b712dd9272979e8b391f8f354
Vitor Lima <vitorsawada2002@gmail.com>
2024-12-26 11:27:58 -0300

    feat: Inicio dos cards do dashboard

---
+++ resources/js/Components/Ui/Cards/CardDashSells.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="bg-white py-2 w-[224px] h-20 rounded-md px-3">
+        <div> <h3 class="text-xs text-gray-500">Total sells</h3> </div>
+        <div>
+            <div>
+                
+                <h4>$126.500</h4>
+            </div>
+        </div>
+        <div>
+            <!-- faça um icone de pop-out para abrir algo em outra guia -->
+            <i class="fas fa-chart-line"></i>
+
+        </div>
+   </div>
+</template>
