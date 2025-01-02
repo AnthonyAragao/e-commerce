69d6273e43241f8f03a211830c10abed9fcffb1d
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-02 13:59:29 -0300

    :sparkles: feat: card dos ultimos clientes

---
+++ resources/js/Components/Ui/Cards/CardLatestClients.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex gap-2 items-center">
+        <img src="../../../../../public/img/exemple.png" class="size-8 rounded-md" alt="">
+        <div>
+                <div class="flex gap-2 justify-between items-center font-medium">
+                    <span class="text-[13px] ">
+                        Esther Howard
+                    </span>
+                    <span class="text-[11px]">
+                        $1,200
+                    </span>
+                </div>
+            <div class="flex text-gray-500 dark:text-gray-400 gap-2 justify-between items-center text-[11px]">
+                <span >Paypal</span>
+                <span>854 Buy's</span>
+            </div>
+        </div>
+    </div>
+</template>
