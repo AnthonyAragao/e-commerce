b419bfe5a75f348f35a8a4869a6046e002bb4aeb
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-03 11:38:18 -0300

    :sparkles: feat: esqueleto de grafico de linhas criado.

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -5,6 +5,51 @@ import CardDashSells from "../../Components/Ui/Cards/CardDashSells.vue";
 import CardLayout from "../../Components/Ui/Cards/CardLayout.vue";
 import CardLatestClients from "../../Components/Ui/Cards/CardLatestClients.vue";
 import TableRecentPurchases from "../../Components/Ui/Tables/TableRecentPurchases.vue";
+import { onMounted } from "vue";
+
+
+
+
+onMounted(() => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['0', '5', '10', '15', '20', '25'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)'
+        },
+        {
+          label: '# of Likes',
+          data: [5, 10, 15, 20, 25, 30],
+          borderWidth: 1,
+          borderColor: 'rgba(153, 102, 255, 1)',
+          backgroundColor: 'rgba(153, 102, 255, 0.2)'
+        },
+        {
+          label: '# of Shares',
+          data: [2, 4, 6, 8, 10, 12],
+          borderWidth: 1,
+          borderColor: 'rgba(255, 159, 64, 1)',
+          backgroundColor: 'rgba(255, 159, 64, 0.2)'
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+});
+
 </script>
 
 <template>
@@ -29,25 +74,25 @@ import TableRecentPurchases from "../../Components/Ui/Tables/TableRecentPurchase
                     <div class="flex gap-2 pb-2 text-xs">
                         <button
                             type=""
-                            class="relative before:content-[''] before:w-full before:h-[1px] before:-bottom-4 before:absolute before:bg-purple-600 text-purple-600 before:dark:bg-purple-400 dark:text-purple-400"
-                        >
+                            class="relative before:content-[''] before:w-full before:h-[1px] before:-bottom-4 before:absolute before:bg-purple-600 text-purple-600 before:dark:bg-purple-400 dark:text-purple-400">
                             Today's
                         </button>
                         <button>Monthly</button>
-
                         <i class="text-gray-500 fas fa-ellipsis-v"></i>
                     </div>
                 </div>
-                <div class="w-[980px] text-red-300 mt-4">Grafico Temporario</div>
+                <div class=" text-red-300 mt-4">
+                    <div>
+                        <canvas id="myChart"></canvas>
+                    </div>
+                </div>
             </CardLayout>
-
-            <CardLayout class="w-[400px]">
+            <CardLayout class="w-[33%]">
                 <div>
                     <div class="flex gap-2 justify-between mb-2">
                         Latest Clients
                         <i class="text-gray-500 fas fa-ellipsis-v mt-1"></i>
                     </div>
-
                     <CardLatestClients />
                     <CardLatestClients />
                     <CardLatestClients />
@@ -58,7 +103,6 @@ import TableRecentPurchases from "../../Components/Ui/Tables/TableRecentPurchase
                 </div>
             </CardLayout>
         </div>
-
         <CardLayout class="mt-2">
             <div>
                 <p>Compras Recentes</p>
@@ -70,3 +114,4 @@ import TableRecentPurchases from "../../Components/Ui/Tables/TableRecentPurchase
         </div>
     </AdminLayout>
 </template>
+
