9e49d1ba33894801d1813e1c77bc1279b75a0659
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-07 15:58:32 -0300

    :sparkles: feat: aba inicial inicializada

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -1,9 +1,18 @@
 <script setup>
+    import UserLayout from "../../Components/Layouts/UserLayout.vue";
     const {products} = defineProps(["products"])
 </script>
 
 <template>
-    <h1>Produtos</h1>
 
-    {{ products }}
+
+    <UserLayout>
+        <H1>Olá</H1>
+
+    </UserLayout>
+
 </template>
+
+
+
+
