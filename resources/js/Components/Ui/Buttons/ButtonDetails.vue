434abd8e6634aa225ef9e652839a07bac135294a
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-07 13:49:31 -0300

    :sparkles: feat: criação do component para visualizar as informações

---
+++ resources/js/Components/Ui/Buttons/ButtonDetails.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+    const {href} = defineProps(['href']);
+
+
+</script>
+<template>
+    <Link
+        :href="href"
+        class="block px-4 text-gray-400 dark:text-gray-300 text-xs font-semibold"
+    >
+        View details
+    </Link>
+</template>
