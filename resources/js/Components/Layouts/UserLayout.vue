84db6f0beaba46e264f9dfc9936e4d8de8fb05a3
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-13 12:26:43 -0300

    :sparkles: feat: footer e estilização do main adicionados ao userlayout

--- resources/js/Components/Layouts/UserLayout.vue
+++ resources/js/Components/Layouts/UserLayout.vue
@@ -1,9 +1,14 @@
 <script setup>
     import HeaderUser from "../../Components/Navigation/HeaderUser.vue";
+    import Footer from '../Layouts/Footer.vue';
 </script>
 
 <template>
-    <HeaderUser />
 
-    <slot></slot>
+    <HeaderUser />
+    <main class="p-7 mt-16 min-h-[calc(100vh-4rem)]">
+                <slot></slot>
+            </main>
+    <Footer/>
+   
 </template>
