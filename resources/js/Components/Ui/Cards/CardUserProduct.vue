8572f593fb35d987675f2e1b15d88e40324b9ce4
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-15 14:17:36 -0300

    :sparkles: feat: carrinho e fav card user product

--- resources/js/Components/Ui/Cards/CardUserProduct.vue
+++ resources/js/Components/Ui/Cards/CardUserProduct.vue
@@ -1,13 +1,16 @@
 <script setup>
-    import { ref } from "vue";
+import { ref } from "vue";
 
-    const { products } = defineProps(["products"]);
+const { products } = defineProps(["products"]);
 
-    const rating = ref(2);
 
-    const setRating = (value) => {
-        rating.value = value;
-    };
+const rating = ref(2);
+
+const setRating = (value) => {
+    rating.value = value;
+};
+
+
 </script>
 
 <template>
@@ -17,16 +20,30 @@
         <div
             v-for="product in products.data"
             :key="product.id"
-            class="bg-white shadow-md rounded-lg p-4 h-[480px] overflow-hidden"
+            class="bg-white shadow-md rounded-lg p-4 h-[480px] overflow-hidden relative"
         >
-            <div class="flex items-center">
-                <i
-                    v-for="star in 5"
-                    :key="star"
-                    class="fas fa-star text-gray-400"
-                    :class="{ 'text-yellow-500': rating >= star }"
-                    @click="setRating(star)"
-                ></i>
+            <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                    <i
+                        v-for="star in 5"
+                        :key="star"
+                        class="fas fa-star text-gray-400"
+                        :class="{ 'text-yellow-500': rating >= star }"
+                        @click="setRating(star)"
+                    ></i>
+                </div>
+                <div class="flex items-center text-xl">
+                    <!-- carrinho e um coração -->
+                    <i class="fas fa-shopping-cart text-gray-400 hover:text-yellow-500 cursor-pointer"></i>
+                    <button
+
+                    >
+                        <i
+                            class="fa-solid fa-heart text-gray-400 ml-2 hover:text-yellow-500 "
+
+                        ></i>
+                    </button>
+                </div>
             </div>
 
             <img
@@ -40,12 +57,15 @@
                     {{ product.name }}
                 </h2>
 
-                <div class="mt-4 flex justify-between items-center">
-                    <span class="text-xl font-semibold text-gray-800">
-                        {{product.price}}
-                    </span>
-                    <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-                        Add to Cart
+                <span class="text-base font-semibold text-gray-600">
+                    {{ product.sale_price }}
+                </span>
+                <div class="absolute bottom-4 left-1/2 -translate-x-1/2 w-[90%]">
+                    <button
+                        class="px-4 py-2 bg-orange-500 w-full text-white rounded-md hover:bg-blue-600"
+                    >
+                    <i class="fas fa-shopping-cart text-white "></i>
+                        Buy
                     </button>
                 </div>
             </div>
