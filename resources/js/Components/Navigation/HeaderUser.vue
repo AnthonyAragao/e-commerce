d543286abcd0a63be9b5ab3e3ee68cefb168363d
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-20 10:29:58 -0300

    :recycle: refactor: ajustado o layout e o estilo do cabeçalho do usuário para melhorar a consistência visual

--- resources/js/Components/Navigation/HeaderUser.vue
+++ resources/js/Components/Navigation/HeaderUser.vue
@@ -14,32 +14,32 @@
 </script>
 
 <template>
-    <Nav class="bg-[#F5F5FA] h-[88px] flex items-center z-10 w-full">
+    <Nav class="bg-[#F5F5FA] h-[88px] flex items-center z-10 w-full ">
         <div
-            class="flex h-[80%] dark:bg-zinc-900 justify-between w-[1420px] mx-auto p-4 items-center gap-2"
+            class="flex h-[80%] dark:bg-zinc-900 justify-between w-[1420px] mx-auto  p-2 items-center gap-2"
         >
             <button
                 type="button"
-                class="flex items-center justify-center h-12 w-12 rounded-md bg-[#ffff] dark:bg-[#313442] dark:text-white border border-gray-200/70"
+                class="flex items-center justify-center h-12 w-12 rounded-[4px] bg-[#ffff] dark:bg-[#313442] dark:text-white border border-gray-200/70"
             >
                 <i class="fas fa-bars"></i>
             </button>
 
             <div
-                class="flex h-12 w-[70%] justify-center px-3 rounded-md relative"
+                class="flex-1 h-12  justify-center  rounded-[4px] relative"
             >
-                <div class="absolute inset-y-0 left-0 flex items-center pl-6 pointer-events-none">
+                <div class="absolute inset-y-0 -left-3 flex items-center pl-6 pointer-events-none">
                     <i class="fas fa-search text-gray-400"></i>
                 </div>
                 <input
                     placeholder="Search on the store"
                     type="search"
-                    class="py-[14px] pl-8 pr-2 bg-[#ffff] text-sm rounded-md w-full focus:outline-none border border-gray-200/70 dark:bg-[#313442] dark:text-white"
+                    class="py-[14px] pl-8 pr-2 bg-[#ffff] text-sm rounded-[4px] w-full focus:outline-none border border-gray-200/70 dark:bg-[#313442] dark:text-white"
                 />
             </div>
 
             <div
-                class="bg-[#ffff] p-3 rounded-md h-12 text-[13px] flex flex-col font-bold text-gray-500 justify-center border border-gray-200/70 text-xs"
+                class="bg-[#ffff] p-3 rounded-[4px] h-12 text-[13px] flex flex-col font-bold text-gray-500 justify-center border border-gray-200/70 text-xs"
             >
                 <div class="flex gap-1">
                     <a class="hover:text-gray-800 font-bold" href="">SIGN IN</a>
@@ -49,7 +49,7 @@
                 <a class="hover:text-gray-800" href="#">SIGN UP</a>
             </div>
 
-            <div class="flex gap-10 h-12 items-center rounded-md bg-[#fff] p-4 border border-gray-200/70">
+            <div class="flex gap-10 h-12 items-center rounded-[4px] bg-[#fff] p-4 border border-gray-200/70">
                 <a href="#" class="relative group">
                     <i class="fas fa-headset"></i>
                     <span class="infotag">SAC</span>
@@ -71,32 +71,32 @@
         :class="{'translate-y-0': showFixedHeader}"
     >
         <div
-            class="flex h-[80%] dark:bg-zinc-900 justify-between w-[1420px] mx-auto p-4 items-center gap-2 bo"
+            class="flex h-[80%] dark:bg-zinc-900 justify-between w-[1420px] mx-auto p-2 items-center gap-2 bo"
         >
             <button
                 type="button"
-                class="flex items-center justify-center h-12 w-12 rounded-md bg-[#ffff] dark:bg-[#313442] dark:text-white border border-gray-200/70"
+                class="flex items-center justify-center h-12 w-12 rounded-[4px] bg-[#ffff] dark:bg-[#313442] dark:text-white border border-gray-200/70"
             >
                 <i class="fas fa-bars"></i>
             </button>
 
             <div
-                class="flex h-12 w-[70%] justify-center px-3 rounded-md relative"
+                class="flex-1 h-12  justify-center  rounded-[4px] relative"
             >
                 <div
-                    class="absolute inset-y-0 left-0 flex items-center pl-6 pointer-events-none"
+                    class="absolute inset-y-0 -left-3 flex items-center pl-6 pointer-events-none"
                 >
                     <i class="fas fa-search text-gray-400"></i>
                 </div>
                 <input
                     placeholder="Search on the store"
                     type="search"
-                    class="py-[14px] pl-8 pr-2 bg-[#ffff] text-sm rounded-md w-full focus:outline-none border border-gray-200/70 dark:bg-[#313442] dark:text-white"
+                    class="py-[14px] pl-8 pr-2 bg-[#ffff] text-sm rounded-[4px] w-full focus:outline-none border border-gray-200/70 dark:bg-[#313442] dark:text-white"
                 />
             </div>
 
             <div
-                class="bg-[#ffff] p-3 rounded-md h-12 text-[13px] flex flex-col font-bold text-gray-500 justify-center border border-gray-200/70 text-xs"
+                class="bg-[#ffff] p-3 rounded-[4px] h-12 text-[13px] flex flex-col font-bold text-gray-500 justify-center border border-gray-200/70 text-xs"
             >
                 <div class="flex gap-1">
                     <a class="hover:text-gray-800" href="">SIGN IN</a>
@@ -106,7 +106,7 @@
                 <a class="hover:text-gray-800" href="#">SIGN UP</a>
             </div>
 
-            <div class="flex gap-10 h-12 items-center rounded-md bg-[#fff] p-4 border border-gray-200/70">
+            <div class="flex gap-10 h-12 items-center rounded-[4px] bg-[#fff] p-4 border border-gray-200/70">
                 <a href="#" class="relative group">
                     <i class="fas fa-headset"></i>
                     <span class="infotag">SAC</span>
