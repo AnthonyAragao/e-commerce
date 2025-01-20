89e753187f39f5a141004ac3306caa3aa3cbcba8
Vitor Lima <vitorsawada2002@gmail.com>
2025-01-20 10:30:14 -0300

    :recycle: refactor: ajustado o estilo do componente de categorias para melhorar a consistência visual das bordas

--- resources/js/Pages/Products/Category.vue
+++ resources/js/Pages/Products/Category.vue
@@ -8,7 +8,7 @@
 <template>
     <UserLayout>
         <div class="pb-10 max-w-[1400px] mx-auto grid grid-cols-5 gap-4">
-            <aside class="col-span-1 bg-white shadow-sm rounded-md p-4">
+            <aside class="col-span-1 bg-white shadow-sm rounded-[4px] p-4">
                 <h2 class="text-lg font-semibold text-gray-900">Related categories</h2>
                 <ul class="mt-6">
                     <li
