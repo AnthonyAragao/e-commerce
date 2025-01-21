bed953209a2c23c9cd62c9c0d3f7438fc47ff9f9
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2025-01-21 09:56:45 -0300

    :sparkles: feat: adicionado componente SidebarLink para os links de navegação

---
+++ resources/js/Components/Navigation/Sidebar/SidebarLink.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const { link, isActive, icon, text } = defineProps(['link', 'isActive', 'icon', 'text']);
+</script>
+
+<template>
+    <Link
+        :href="link"
+        class="grid grid-cols-6 px-2 py-3 rounded-xl items-center  hover:bg-[#7364DB] hover:text-white transition duration-300"
+        :class="{'bg-[#7364DB] text-white': isActive}"
+    >
+        <i :class="['mx-auto', icon]"></i>
+        <span>{{ text }}</span>
+    </Link>
+</template>
