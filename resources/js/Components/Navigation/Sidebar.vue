c077016b33ddb5b3755fbee8c10c6c9a4b247a19
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2025-01-21 09:33:06 -0300

    :recycle: refactor: ajustado o layout do Sidebar para utilizar grid, melhorando a organização e a responsividade dos itens de navegação

--- resources/js/Components/Navigation/Sidebar.vue
+++ resources/js/Components/Navigation/Sidebar.vue
@@ -33,61 +33,62 @@
             <nav class="flex flex-col py-4 space-y-2 cursor-pointer text-sm font-semibold mt-4">
                 <Link
                     href="/admin/dashboard"
-                    class="px-4 py-3 rounded-xl flex items-center space-x-2 hover:bg-[#7364DB] hover:text-white transition duration-300"
+                    class="grid grid-cols-6 px-2 py-3 rounded-xl items-center  hover:bg-[#7364DB] hover:text-white transition duration-300"
                     :class="{'bg-[#7364DB] text-white': $page.url.startsWith('/admin/dashboard')}"
                 >
-                    <i class="fa-solid fa-chart-line"></i> <span>Dashboard</span>
+                    <i class="fa-solid fa-chart-line mx-auto"></i>
+                    <span>Dashboard</span>
                 </Link>
 
                 <Link
                     href="/admin/products"
-                    class="px-4 py-3 rounded-xl flex items-center space-x-2 hover:bg-[#7364DB] hover:text-white transition duration-300"
+                    class="grid grid-cols-6 px-2 py-3 rounded-xl items-center hover:bg-[#7364DB] hover:text-white transition duration-300"
                     :class="{'bg-[#7364DB] text-white': $page.url.startsWith('/admin/products')}"
                 >
-                    <i class="fas fa-box"></i> <span>Products</span>
+                    <i class="fas fa-box mx-auto"></i>
+                    <span>Products</span>
                 </Link>
 
                 <Link
                     href="/admin/orders"
-                    class="px-4 py-3 rounded-xl flex items-center space-x-2 hover:bg-[#7364DB] hover:text-white transition duration-300"
+                    class="grid grid-cols-6 px-2 py-3 rounded-xl items-center hover:bg-[#7364DB] hover:text-white transition duration-300"
                     :class="{'bg-[#7364DB] text-white': $page.url.startsWith('/admin/orders')}"
                 >
-                    <i class="fas fa-shopping-cart"></i> <span>Orders</span>
+                    <i class="fas fa-shopping-cart mx-auto "></i>
+                    <span>Orders</span>
                 </Link>
 
                 <Link
                     href="/admin/customers"
-                    class="px-4 py-3 rounded-xl flex items-center space-x-2 hover:bg-[#7364DB] hover:text-white transition duration-300"
+                    class="grid grid-cols-6 px-2 py-3 rounded-xl items-center hover:bg-[#7364DB] hover:text-white transition duration-300"
                     :class="{'bg-[#7364DB] text-white': $page.url.startsWith('/admin/customers')}"
                 >
-                    <i class="fas fa-users"></i> <span>Customers</span>
+
+                    <i st class="fas fa-users mx-auto"></i>
+                    <span>Customers</span>
                 </Link>
 
                 <Link
                     href="/admin/settings"
-                    class="px-4 py-3 rounded-xl flex items-center space-x-2 hover:bg-[#7364DB] hover:text-white transition duration-300"
+                    class="grid grid-cols-6 px-2 py-3 rounded-xl items-center hover:bg-[#7364DB] hover:text-white transition duration-300"
                     :class="{'bg-[#7364DB] text-white': $page.url.startsWith('/admin/settings')}"
                 >
-                    <i class="fas fa-cog"></i> <span>Settings</span>
+                    <i class="fas fa-cog mx-auto"></i>
+                    <span>Settings</span>
                 </Link>
-
             </nav>
         </div>
-
 
         <div class="p-4">
             <div class="flex justify-between items-center bg-[#E8EDF2] py-4 px-12 rounded-xl dark:bg-[#313442]">
                 <i class="fas fa-moon text-purple-500"></i>
 
                 <div
-                    class="relative w-12 h-4 bg-gray-300 rounded-full cursor-pointer flex items-center transition-all duration-300 dark:bg-[#2C2C35]"
+                    class="relative w-12 h-4 bg-gray-300 rounded-full cursor-pointer grid grid-cols-6 items-center transition-all duration-300 dark:bg-[#2C2C35]"
                     @click="toggleDarkMode"
                 >
                     <div
-                        :class="[
-                            'absolute w-5 h-5  rounded-full transition-all duration-500',
-                            ballPosition
-                        ]"
+                        :class="['absolute w-5 h-5  rounded-full transition-all duration-500', ballPosition]"
                     >
                     </div>
                 </div>
