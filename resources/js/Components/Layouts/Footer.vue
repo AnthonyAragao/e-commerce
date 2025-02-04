53773038e8730b302f2ad61908c5da7497e1eece
Vitor Lima <vitorsawada2002@gmail.com>
2025-02-04 10:30:11 -0300

    :iphone: feat: ajustar layout do rodapé para melhor responsividade

--- resources/js/Components/Layouts/Footer.vue
+++ resources/js/Components/Layouts/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-     <footer class="dark:bg-[#18181b]  dark:text-gray-200 flex justify-center relative before:w-full before:h-[1px] before:-top-2 before:absolute before:bg-gray-200 dark:before:bg-gray-700 mx-7 mb-3" >
+     <footer class="dark:bg-[#18181b] dark:text-gray-200 max-md:flex-col max-md:items-end flex justify-center relative before:w-full before:h-[1px] before:-top-2 before:absolute before:bg-gray-200 dark:before:bg-gray-700 mx-7 max-md:mx-4 mb-3" >
         <p class="text-sm py-2 w-full dark:text-gray-400 text-gray-500 ">
             <a>
             <span>©</span> <span>M</span><span>a</span><span>d</span><span>e</span> <span>I</span><span>n</span> <span>2</span><span>0</span><span>2</span><span>5</span> <span>-</span> <span>b</span><span>y</span>
@@ -24,7 +24,7 @@
             </a>
         </span>
         </p>
-        <p class="flex gap-4 text-sm text-gray-500 dark:text-gray-400 ">
+        <p class="flex gap-4 text-sm text-gray-500 dark:text-gray-400">
             <button class="hover:text-gray-950 dark:hover:text-gray-200">About</button>
             <button class="hover:text-gray-950 dark:hover:text-gray-200">Careers</button>
         </p>
