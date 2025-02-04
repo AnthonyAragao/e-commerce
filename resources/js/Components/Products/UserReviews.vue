7829eaf88443ec9c82ed2e54e35d548dbd48981c
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2025-02-04 11:23:10 -0300

    :sparkles: feat: adicionado componentes para exibir produtos da mesma categoria e avaliações de usuários na página de detalhes do produto

---
+++ resources/js/Components/Products/UserReviews.vue
@@ -0,0 +1,58 @@
+<script setup>
+    const { product } = defineProps(['product']);
+</script>
+
+<template>
+    <div class="max-w-[1400px] mx-auto my-10">
+        <div class="flex items-center gap-2">
+            <i class="fas fa-star text-primary"></i>
+            <h2 class="text-2xl font-bold text-gray-700 uppercase">user reviews</h2>
+        </div>
+
+        <div class="flex items-end gap-2 text-primary">
+            <h2 class="text-6xl font-bold">
+                {{ product.rating }}
+            </h2>
+            <p class="text-xl mb-1">/5</p>
+        </div>
+
+        <p class="text-sm text-gray-700">({{ product.review_count }}) reviews</p>
+
+        <div class="grid grid-cols-2 gap-x-6">
+            <div
+                v-for="(review, index) in product.reviews.slice(0, 4)"
+                :key="index"
+                class="rounded-3xl w-full bg-white p-4 mt-4 min-h-44 shadow-card-reviews border border-[rgb(242, 243, 244)]"
+            >
+                <p class="text-sm text-black font-semibold">{{ review.user.name }}</p>
+
+                <div class="flex flex-col gap-1 mt-2">
+                    <div class="flex items-center gap-1">
+                        <i
+                            v-for="star in 5"
+                            :key="star"
+                            :class="{
+                                'fas fa-star' : review.rating >= star,
+                                'far fa-star' : review.rating < star
+                            }"
+                            class="text-primary"
+                        ></i>
+                    </div>
+
+                    <p class="text-xs text-gray-400">Reviewed on {{ new Date(review.created_at).toLocaleDateString() }}</p>
+                </div>
+
+                <p class="text-sm text-gray-500 mt-2">
+                    {{ review.comment }}
+                </p>
+            </div>
+        </div>
+
+        <div
+            v-if="product.reviews.length > 4"
+            class="w-full text-center mt-10"
+        >
+            <a href="#" class="text-primary text-sm mt-4 font-bold uppercase">See all reviews</a>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Products/Show.vue
+++ resources/js/Pages/Products/Show.vue
@@ -1,6 +1,8 @@
 <script setup>
     import UserLayout from '../../Components/Layouts/UserLayout.vue';
     import CardProduct from '../../Components/Ui/Cards/CardProduct.vue';
+    import ProductsFromTheSameCategory from '../../Components/Products/ProductsFromTheSameCategory.vue';
+    import UserReviews from '../../Components/Products/UserReviews.vue';
 
     const { product, relatedProducts } = defineProps([ 'product', 'relatedProducts']);
 </script>
@@ -66,12 +68,12 @@
                 </div>
 
                 <div class="flex flex-col mt-2">
-                    <p class="text-xs text-gray-400">
+                    <p class="text-xs text-gray-500">
                         From: <span class="line-through text-red-600/70">R$ {{ product.regular_price }}</span> per:
                     </p>
 
                     <span class="text-5xl font-bold text-primary">R$ {{ product.sale_price }}</span>
-                    <span class="text-gray-400">10x of R$ {{ (product.sale_price / 10).toFixed(2).replace('.', ',') }}</span>
+                    <span class="text-gray-500">10x of R$ {{ (product.sale_price / 10).toFixed(2).replace('.', ',') }}</span>
                 </div>
 
                 <div class="flex items-center gap-4 mt-4 w-full">
@@ -90,70 +92,13 @@
             </div>
         </div>
 
-        <!-- products from the same category -->
-        <div class="max-w-[1400px] mx-auto my-8">
-            <div class="flex items-center gap-2">
-                <i class="fas fa-box-open text-primary"></i>
-                <h2 class="text-2xl font-bold text-gray-700 uppercase">products from the same category</h2>
-            </div>
+        <ProductsFromTheSameCategory
+            :products="relatedProducts"
+        />
 
-            <div class="grid grid-cols-4 gap-4 mt-4">
-                <div class="col-span-4 grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-4">
-                    <CardProduct
-                        v-for="product in relatedProducts"
-                        :key="product.id"
-                        :product="product"
-                    />
-                </div>
-            </div>
-        </div>
-
-        <!-- Reviews section -->
-        <div class="max-w-[1400px] mx-auto my-8">
-            <div class="flex items-center gap-2">
-                <i class="fas fa-star text-primary"></i>
-                <h2 class="text-2xl font-bold text-gray-700 uppercase">user reviews</h2>
-            </div>
-
-            <div class="flex items-end gap-2 text-primary">
-                <h2 class="text-6xl font-bold">
-                    {{ product.rating }}
-                </h2>
-                <p class="text-xl mb-1">/5</p>
-            </div>
-
-            <p class="text-sm text-gray-700">({{ product.review_count }}) reviews</p>
-
-            
-            <div class="grid grid-cols-2 gap-x-6">
-                <div
-                    v-for="review in 4"
-                    :key="review"
-                    class="rounded-3xl w-full bg-white p-4 mt-4 shadow-card-reviews border border-[rgb(242, 243, 244)]"
-                >
-                    <p class="text-sm text-black font-semibold">John Doe</p>
-
-                    <div class="flex flex-col gap-1 mt-2">
-                        <div class="flex items-center gap-1">
-                            <i class="fas fa-star text-primary"></i>
-                            <i class="fas fa-star text-primary"></i>
-                            <i class="fas fa-star text-primary"></i>
-                            <i class="fas fa-star text-primary"></i>
-                            <i class="far fa-star text-primary"></i>
-                        </div>
-
-                        <p class="text-xs text-gray-400">Reviewed on 2021-09-01</p>
-                    </div>
-
-                    <p class="text-sm text-gray-500 mt-2">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-                        purus feugiat, molestie ipsum et, consectetur odio. Nullam sit amet
-                        semper nulla. Nulla facilisi. Nullam nec nisi ac nunc ultricies
-                        tincidunt. Nullam sit amet semper nulla. Nulla facilisi. Nullam nec
-                        nisi ac nunc ultricies tincidunt.
-                    </p>
-                </div>
-            </div>
-        </div>
+        <!-- User reviews -->
+        <UserReviews
+            :product="product"
+        />
     </UserLayout>
 </template>
