3d74fabebaa1ae5d6d5689c3ef45173b3ca8798e
Vitor Lima <vitorsawada2002@gmail.com>
2025-02-05 10:52:00 -0300

    :lipstick: feat: dark adicionado a sidebar da category product

--- resources/js/Pages/Products/Category.vue
+++ resources/js/Pages/Products/Category.vue
@@ -10,13 +10,14 @@
     <UserLayout>
         <div class="pb-10 max-w-[1400px] mx-auto grid grid-cols-5 gap-4">
             <aside class="col-span-1 flex flex-col gap-4">
-                <div class="bg-white shadow-sm rounded-[4px] p-4 h-fit">
-                    <h2 class="text-lg font-semibold text-gray-700">Categories list</h2>
+                <div class="bg-white  border-[1.5px] border-gray-200 dark:border-gray-700 dark:bg-[#1F2128] shadow-sm rounded-[4px] p-4 h-fit">
+                    <h2 class="text-lg font-semibold text-gray-700 dark:text-gray-300">Categories list</h2>
                     <ul class="mt-2">
                         <li
                             v-for="category in categories"
                             :key="category.id"
-                            class="mt-1 text-gray-500 text-sm hover:text-gray-800 cursor-pointer"
+                            class="mt-1 text-gray-500 text-sm hover:text-gray-800 cursor-pointer
+                            dark:text-gray-400 dark:hover:text-gray-300"
                         >
                             <Link :href="`/categories/${category.slug}`">
                                 {{ category.name }}
@@ -25,9 +26,9 @@
                     </ul>
                 </div>
 
-                <div class="bg-white shadow-sm rounded-[4px] p-4 h-fit">
-                    <h2 class="text-lg font-semibold text-gray-700">Price</h2>
-                    <p class="text-sm mt-2 text-gray-500">Price range</p>
+                <div class="bg-white  border-[1.5px] border-gray-200 dark:border-gray-700 dark:bg-[#1F2128] shadow-sm rounded-[4px] p-4 h-fit">
+                    <h2 class="text-lg font-semibold text-gray-700 dark:text-gray-300">Price</h2>
+                    <p class="text-sm mt-2 text-gray-500 dark:text-gray-400">Price range</p>
 
                     <div class="mt-2">
                         <!-- add here with library noUiSlider -->
