d1d7d9d3cf385b46ff09c2c7e626655799c5d131
Vitor Lima <vitorsawada2002@gmail.com>
2025-02-05 10:03:14 -0300

    :sparkles: feat: criação do component toggle dark mode

---
+++ resources/js/Components/Ui/Buttons/ButtonDarkMode.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+// Configuração do modo escuro
+const isDarkMode = useDark({
+    storageKey: "dark-mode",
+    valueDark: "dark",
+    valueLight: "light",
+});
+
+const toggleDarkMode = useToggle(isDarkMode);
+
+</script>
+
+
+<template>
+    <button
+            @click="toggleDarkMode()"
+            class=" p-2 size-12 rounded-lg bg-white  border-[1.5px] border-gray-200 dark:border-gray-700 dark:bg-[#1F2128]"
+        >
+            {{ isDarkMode ? "🌙" : "☀️" }}
+    </button>
+</template>
