eb4ef0dd3bef4a3a7248261267c71369a4e32594
AnthonyAragao <anthony.amoz.s.a@gmail.com>
2025-02-23 01:09:24 -0300

    :bug: refactor: adicionados componentes ProductSection e PromoCard para exibição de produtos e promoções

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -1,18 +1,19 @@
 <script setup>
     import { provide } from "vue";
     import UserLayout from "@/Components/Layouts/UserLayout.vue";
-    import CardProduct from "@/Components/Ui/Cards/CardProduct.vue";
     import BannerSwiper from "@/Components/Ui/Banner/BannerSwiper.vue";
+    import ProductSection from "@/Components/Products/ProductSection.vue";
+    import PromoCard from "@/Components/Ui/Cards/PromoCard.vue";
 
-    const { products, categories } = defineProps([ "products", "categories" ])
-    provide("categories", categories)
+    const { bestSellersProducts, latestProducts, categories } = defineProps([ "bestSellersProducts", "latestProducts", "categories" ]);
+    provide("categories", categories);
 </script>
 
 <template>
     <Head title="E-commerce" />
 
     <UserLayout>
-        <div class="relative">
+        <div class="relative mb-44">
             <BannerSwiper />
 
             <div class="max-w-[1400px] mx-auto grid grid-cols-6 gap-4 absolute -bottom-32 left-0 right-0 z-10">
@@ -38,67 +39,31 @@
             </div>
         </div>
 
-        <div class="max-w-[1400px] mx-auto mt-44 mb-10 ">
-            <h1 class="text-gray-800 uppercase font-semibold text-2xl dark:text-gray-300 mb-2">
-                <i class="fas fa-fire text-primary dark:text-secondary"></i> best sellers
-            </h1>
+        <ProductSection
+            title="best sellers"
+            icon="fas fa-fire"
+            :products="bestSellersProducts"
+        />
 
-            <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-4">
-                <CardProduct
-                    v-for="(product, index) in products.data.slice(0, 5)"
-                    :key="index"
-                    :product="product"
-                />
-            </div>
-        </div>
+        <ProductSection
+            title="new Arrivals"
+            icon="fas fa-star"
+            :products="latestProducts"
+        />
 
-        <div class="max-w-[1400px] mx-auto  mb-10">
-            <h1 class="text-gray-800 uppercase font-semibold text-2xl dark:text-gray-300 mb-2">
-                <i class="fas fa-star text-primary dark:text-secondary"></i> featured products
-            </h1>
-
-            <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-4">
-                <CardProduct
-                    v-for="(product, index) in products.data.slice(10, 15)"
-                    :key="index"
-                    :product="product"
-                />
-            </div>
-        </div>
-
-
-        <!-- Adicionar em um componente separado depois -->
         <div class="max-w-[1400px] mx-auto mb-20 grid grid-cols-2 gap-4">
-            <div class="h-[300px] bg-white rounded-md shadow-md flex justify-between items-center overflow-hidden border border-gray-100 dark:bg-[#1F2128] dark:border-gray-700">
+            <PromoCard
+                title="Activate Coupons"
+                buttonText="Activate"
+                image="/img/cupon.jpg"
+            />
 
-                <div class="mx-10 w-48">
-                    <h2 class="text-2xl font-semibold text-gray-600 dark:text-white uppercase">
-                        activate coupons
-                    </h2>
-
-                    <button class="mt-4 bg-primary text-white w-full py-2 rounded-md text-sm dark:bg-secondary">
-                        Activate
-                    </button>
-                </div>
-
-                <img src="/img/cupon.jpg" alt=""  class="h-full object-cover object-center w-[300px]">
-            </div>
-
-
-            <div class="h-[300px] bg-white rounded-md shadow-md flex justify-between items-center overflow-hidden border border-gray-100 dark:bg-[#1F2128] dark:border-gray-700">
-                <div class="mx-10 w-48">
-                    <p class="uppercase text-gray-500 font-semibold tracking-[.5rem] text-xs">enjoy</p>
-                    <h2 class="text-2xl font-semibold text-gray-600 dark:text-white uppercase mt-4">
-                        offers of the day
-                    </h2>
-
-                    <button class="mt-4 bg-primary text-white w-full py-2 rounded-md text-sm dark:bg-secondary">
-                        View Offers
-                    </button>
-                </div>
-
-                <img src="/img/offer.jpg" alt="" class="h-full object-cover object-center w-[300px]">
-            </div>
+            <PromoCard
+                subtitle="enjoy"
+                title="offers of the day"
+                buttonText="View Offers"
+                image="/img/offer.jpg"
+            />
         </div>
     </UserLayout>
 </template>
