63b801cb04274d2e0d712d1553549e8dae3c6b40
AnthonyAragao <anthony.amoz.s.a@gmail.com>
2025-02-26 00:05:51 -0300

    :recycle: refactor: adicionado componente FeatureCards para exibir múltiplos FeatureCards na página de produtos

---
+++ resources/js/Components/Ui/Cards/FeatureCards.vue
@@ -0,0 +1,55 @@
+<script setup>
+    import FeatureCard from "@/Components/Ui/Cards/FeatureCard.vue";
+</script>
+
+<template>
+     <div class="max-w-[1400px] mx-auto grid grid-cols-6 gap-4 absolute -bottom-32 left-0 right-0 z-10">
+        <FeatureCard
+            title="Safe and Secure"
+            image="/img/escudo.png"
+            description="You can be sure that your data is safe with us"
+            buttonText="How it works"
+            buttonLink="#"
+        />
+
+        <FeatureCard
+            title="Secure payment"
+            image="/img/pagamento-seguro.png"
+            description="We have the best payment methods for you to make your purchase safely"
+            buttonText="Show more"
+            buttonLink="#"
+        />
+
+        <FeatureCard
+            title="Best sellers"
+            image="/img/carrinho-de-compras.png"
+            description="Check out our best sellers. Take advantage of the discounts."
+            buttonText="Go to the store"
+            buttonLink="#"
+        />
+
+        <FeatureCard
+            title="Our categories"
+            image="/img/opcoes.png"
+            description="We have a wide variety of products for you to choose from"
+            buttonText="View categories"
+            buttonLink="#"
+        />
+
+        <FeatureCard
+            title="Low Prices"
+            image="/img/preco-baixo.png"
+            description="We have the best prices for you to save"
+            buttonText="View More"
+            buttonLink="#"
+        />
+
+        <FeatureCard
+            title="Free shipping"
+            image="/img/frete.png"
+            description="We offer free shipping throughout Brazil"
+            buttonText="Learn more"
+            buttonLink="#"
+        />
+    </div>
+</template>
--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -4,7 +4,7 @@
     import BannerSwiper from "@/Components/Ui/Banner/BannerSwiper.vue";
     import ProductSection from "@/Components/Products/ProductSection.vue";
     import PromoCard from "@/Components/Ui/Cards/PromoCard.vue";
-    import FeatureCard from "@/Components/Ui/Cards/FeatureCard.vue";
+    import FeatureCards from "@/Components/Ui/Cards/FeatureCards.vue";
 
     const { bestSellersProducts, latestProducts, categories } = defineProps([ "bestSellersProducts", "latestProducts", "categories" ]);
     provide("categories", categories);
@@ -16,57 +16,7 @@
     <UserLayout>
         <div class="relative mb-44">
             <BannerSwiper />
-
-            <div class="max-w-[1400px] mx-auto grid grid-cols-6 gap-4 absolute -bottom-32 left-0 right-0 z-10">
-                <!-- Substituir isto por componente depois -->
-                <FeatureCard
-                    title="Safe and Secure"
-                    image="/img/escudo.png"
-                    description="You can be sure that your data is safe with us"
-                    buttonText="How it works"
-                    buttonLink="#"
-                />
-
-                <FeatureCard
-                    title="Secure payment"
-                    image="/img/pagamento-seguro.png"
-                    description="We have the best payment methods for you to make your purchase safely"
-                    buttonText="Show more"
-                    buttonLink="#"
-                />
-
-                <FeatureCard
-                    title="Best sellers"
-                    image="/img/carrinho-de-compras.png"
-                    description="Check out our best sellers. Take advantage of the discounts."
-                    buttonText="Go to the store"
-                    buttonLink="#"
-                />
-
-                <FeatureCard
-                    title="Our categories"
-                    image="/img/opcoes.png"
-                    description="We have a wide variety of products for you to choose from"
-                    buttonText="View categories"
-                    buttonLink="#"
-                />
-
-                <FeatureCard
-                    title="Low Prices"
-                    image="/img/preco-baixo.png"
-                    description="We have the best prices for you to save"
-                    buttonText="View More"
-                    buttonLink="#"
-                />
-
-                <FeatureCard
-                    title="Free shipping"
-                    image="/img/frete.png"
-                    description="We offer free shipping throughout Brazil"
-                    buttonText="Learn more"
-                    buttonLink="#"
-                />
-            </div>
+            <FeatureCards />
         </div>
 
         <ProductSection
